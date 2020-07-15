de0d5e9d061c7b8d33b1e7d1bfaad99be9aab77c
Matt Sirkis <sirkis.matt@gmail.com>
2020-07-15 13:58:08 +1200

    updated html, css, and js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let map
-
+let map 
+let infoWindow = null
 
 function initMap() {
     //initialize map on specific destination
@@ -9,88 +9,72 @@ function initMap() {
     map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: myLatlng});
 }
-    // Create the initial InfoWindow.
-  /*let infoWindow = new google.maps.InfoWindow({
-      content: 'Click the map to see if it is safe to exercise outside!', 
-      position: myLatlng
-    });
-    infoWindow.open(map);*/
-    
-   /*console.log("Lat", latitude);
-    console.log("Lng", longitude);
-if (latitude >= -90) {
-    console.log("Lng", longitude);
-    postInfo(latitude, longitude, map)
-    //infoWindow.close();
-    let contentString = '<div id="content">Oi</div>'
-    // Create a new InfoWindow.
-    let infoWindow = new google.maps.InfoWindow({
-        position: { lat: -34.397, lng: 150.644 },
-        map: map,
-        maxWidth: 200
-    });
-    infoWindow.setContent(contentString);
-    infoWindow.open(map);*/
-
-    /*
-    // Configure the click listener.
-    map.addListener('click', function(mapsMouseEvent) {
-    // Close the current InfoWindow.
-    infoWindow.close();
-   
-    //get coords and create an array
-    let coords = mapsMouseEvent.latLng.toString().split(",");
-    console.log(coords);
-    //grab lat and lon numbers from coords array
-    let lat = parseFloat(coords[0].substring(1));
-    let lon = parseFloat(coords[1]);
-    console.log("Lat", lat);
-    console.log("Lng", lon);
-    //call airByLatLon with lat, lon arguments
-    airByLatLon(lat, lon);
-  });
-
-  //add listener to add an info window
-  map.addListener("click", function(e) {
-    postInfo(e.latLng, map);
-  });*/
+ 
 
 function displayResults(responseJson){
-    console.log(responseJson);
-   console.log(responseJson.data.city.name);
-   console.log(responseJson.data.aqi);
-   console.log(responseJson.data.time.s);
-   //console.log(responseJson.data.city.geo);
-   let coords = responseJson.data.city.geo
-   console.log(coords)
-   //grab lat and lon numbers from coords array
-   let latitude = parseFloat(coords[0]);
-   let longitude = parseFloat(coords[1]);
-   
-    //postInfo(latitude, longitude, map);
-  
-   // if there are previous results, remove them
-    //$('#content').empty();
-
-    let contentString = `<div id="content"><div id="siteNotice"></div><h1 id="firstHeading">${responseJson.data.city.name}
-    </h1><div id="bodyContent"><p>AQI ${responseJson.data.aqi}</p><p>Updated ${responseJson.data.time.s}
-    </p><button id='infoButton'>Click for more info</button>
+console.log(responseJson);
+    //console.log(responseJson.data.city.geo);
+    let coords = responseJson.data.city.geo
+    console.log(coords)
+    let aqi = responseJson.data.aqi
+    //grab lat and lon numbers from coords array
+    let latitude = parseFloat(coords[0]);
+    let longitude = parseFloat(coords[1]);
+    let forecast = responseJson.data.forecast.daily.pm25
+    console.log(forecast)
+    let contentString
+    if (aqi <= 50) {
+    contentString = `<div id="content" class="green"><h3 id="firstHeading">${responseJson.data.city.name}
+    </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Good Quality!</p><p>Updated ${responseJson.data.time.s}
+    </p><button id='infoButton' onClick="${myFunction(forecast)}">Click for more info</button>
     </div></div>`;
+    } else if (aqi >= 51 && aqi <= 100) {
+    contentString = `<div id="content" class="yellow"><h3 id="firstHeading">${responseJson.data.city.name}
+    </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Moderate</p><p>Updated ${responseJson.data.time.s}
+    </p><button id='infoButton' onClick="myFunction()">Click for more info</button>
+    </div></div>`;
+    } else if (aqi >= 101 && aqi <= 150) {
+        contentString = `<div id="content" class="orange"><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Unhealthy for Sensitive Groups</p><p>Updated ${responseJson.data.time.s}
+        </p><button id='infoButton' onClick="myFunction()">Click for more info</button>
+        </div></div>`;
+    } else if (aqi >= 151 && aqi <= 200) {
+        contentString = `<div id="content" class="red"><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Unhealthy</p><p>Updated ${responseJson.data.time.s}
+        </p><button id='infoButton' onClick="myFunction()">Click for more info</button>
+        </div></div>`;
+    } else if (aqi >= 201 && aqi < 300) {
+        contentString = `<div id="content" class="purple"><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Very Unhealthy</p><p>Updated ${responseJson.data.time.s}
+        </p><button id='infoButton' onClick="myFunction()">Click for more info</button>
+        </div></div>`;
+    } else {
+        contentString = `<div id="content" class="really-red"><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Hazardous</p><p>Updated ${responseJson.data.time.s}
+        </p><button id='infoButton' onClick="myFunction()">Click for more info</button>
+        </div></div>`;
+    }
+    
     postInfo(latitude, longitude, map, contentString);
 
+}
+function myFunction(forecast){
+    
+    console.log(forecast)
 }
 
 //Info window function
 function postInfo(latitude, longitude, map, contentString) {
+    
     if (infoWindow){
        infoWindow.close();
-    }
+    };
     console.log("Lat", latitude);
    console.log("Lng", longitude);
   
     // Create a new InfoWindow.
    
-    let infoWindow = new google.maps.InfoWindow({
+    infoWindow = new google.maps.InfoWindow({
         position: { lat: latitude, lng: longitude },
         map: map,
         maxWidth: 200
@@ -98,8 +82,12 @@ function postInfo(latitude, longitude, map, contentString) {
     });
     infoWindow.setContent(contentString);
     infoWindow.open(map);
-  }
-      
+  
+    
+}
+/*$( "#infoButton" ).click(function() {
+    console.log('clicked');
+  });    */
 
 
 //Get Air quality by City
@@ -108,6 +96,7 @@ function getAir(searchTerm){
     fetch(`https://api.waqi.info/feed/${searchTerm}/?token=`)
     .then(response => {
         if (response.ok) {
+        $('#js-error-message').empty();
         return response.json();
         }
         throw new Error(response.statusText);
@@ -115,7 +104,7 @@ function getAir(searchTerm){
     //Pass json results to displayResults function
     .then(responseJson => displayResults(responseJson))
     .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        $('#js-error-message').text(`Sorry we don't have a reading of that City!`);
     }); 
 }
 //getAir()
