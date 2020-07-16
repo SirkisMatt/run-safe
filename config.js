96fb223e44695ab47b60351f1d629d9e5b334e63
Matt Sirkis <sirkis.matt@gmail.com>
2020-07-16 17:11:18 +1200

    Added table that displays AQI Forecast

---
+++ config.js
@@ -0,0 +1,4 @@
+let config = {
+    MAP_KEY : '',
+    KEY_2 : ''
+  }
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+var mapKey = config.MAP_KEY;
+var otherKey = config.KEY_2;
 //map and infoWindow set to global scope
 let map 
 let infoWindow = null
@@ -8,7 +10,7 @@ function initMap() {
     let myLatlng = {lat: -41.269, lng: 173.285};
     map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: myLatlng});
-}
+};
 
 //responseJson and forecast set to global scope
 let responseJson
@@ -33,63 +35,70 @@ console.log(responseJson);
     //if else to add class="color" depending on aqi
     //onClick="pm25Forecast()" to add forecast under map
     if (aqi <= 50) {
-    contentString = `<div id="content" class="green"><h3 id="firstHeading">${responseJson.data.city.name}
-    </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Good Quality!</p><p>Should be good for a run!</p><p>Updated ${responseJson.data.time.s}
+    contentString = `<div id="content" ><h3 id="firstHeading">${responseJson.data.city.name}
+    </h3><div id="bodyContent"><p class="green">AQI ${aqi}</p><p>Good Quality!</p><p>Should be good for a run!</p><p>Updated ${responseJson.data.time.s}
     </p><button id='infoButton' onClick="pm25Forecast()">Click for forecast</button>
     </div></div>`;
     } else if (aqi >= 51 && aqi <= 100) {
-    contentString = `<div id="content" class="yellow"><h3 id="firstHeading">${responseJson.data.city.name}
-    </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Moderate</p><p>Updated ${responseJson.data.time.s}
+    contentString = `<div id="content" ><h3 id="firstHeading">${responseJson.data.city.name}
+    </h3><div id="bodyContent"><p class="yellow">AQI ${aqi}</p><p>Moderate</p><p>Updated ${responseJson.data.time.s}
     </p><button id='infoButton' onClick="pm25Forecast()">Click for forecast</button>
     </div></div>`;
    
     } else if (aqi >= 101 && aqi <= 150) {
-        contentString = `<div id="content" class="orange"><h3 id="firstHeading">${responseJson.data.city.name}
-        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Unhealthy for Sensitive Groups</p><p>Updated ${responseJson.data.time.s}
+        contentString = `<div id="content" ><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p class="orange">AQI ${aqi}</p><p>Unhealthy for Sensitive Groups</p><p>Updated ${responseJson.data.time.s}
         </p><button id='infoButton' onClick="pm25Forecast()">Click for forecast</button>
         </div></div>`;
     } else if (aqi >= 151 && aqi <= 200) {
-        contentString = `<div id="content" class="red"><h3 id="firstHeading">${responseJson.data.city.name}
-        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Unhealthy</p><p>Updated ${responseJson.data.time.s}
+        contentString = `<div id="content" ><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p class="red">AQI ${aqi}</p><p>Unhealthy</p><p>Updated ${responseJson.data.time.s}
         </p><button id='infoButton' onClick="pm25Forecast()">Click for forecast</button>
         </div></div>`;
     } else if (aqi >= 201 && aqi < 300) {
-        contentString = `<div id="content" class="purple"><h3 id="firstHeading">${responseJson.data.city.name}
-        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Very Unhealthy</p><p>Updated ${responseJson.data.time.s}
+        contentString = `<div id="content" ><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p class="purple">AQI ${aqi}</p><p>Very Unhealthy</p><p>Updated ${responseJson.data.time.s}
         </p><button id='infoButton' onClick="pm25Forecast()">Click for forecast</button>
         </div></div>`;
     } else {
-        contentString = `<div id="content" class="really-red"><h3 id="firstHeading">${responseJson.data.city.name}
-        </h3><div id="bodyContent"><p>AQI ${aqi}</p><p>Hazardous</p><p>Updated ${responseJson.data.time.s}
+        contentString = `<div id="content"><h3 id="firstHeading">${responseJson.data.city.name}
+        </h3><div id="bodyContent"><p class="really-red">AQI ${aqi}</p><p>Hazardous</p><p>Updated ${responseJson.data.time.s}
         </p><button id='infoButton' onClick="pm25Forecast()">Click for forecast</button>
         </div></div>`;
     }
     //Create infoWindow and place on map in City's coords
     postInfo(latitude, longitude, map, contentString);
-}
+};
 
 //function to add forecast if user clicks for forecast in infoWindow
 function pm25Forecast(){
-  
-    console.log(forecast)
     //empty more-info if there is anything
     $('#more-info').empty();
-    //add unique id to each date
-    let id = 0
-    
-    //iterate through forecast array and 
-       let mapped =  forecast.map(product => {  
-        return `
-      <div id="${id++}"><p>Date: ${ product.day }<p>
-      <p>pm25 Average: ${ product.avg }<p>
-      <p>pm25 Max: ${ product.max }<p>
-      <p>pm25 Min: ${ product.min }<p></div> <br>`;
-  }).join('');
-        console.log(mapped);
-    
-    //Display mapped results in the DOM
-    $('#more-info').append(`<h3>${searchTerm} pm25 Forecast</h3><br>` + mapped);
-}
+   
+let table = $("<table></table>");
+let thead = $("<thead></thead>");
+thead.append($("<th></th>").html("Date"));
+thead.append($("<th></th>").html("pm25 Average"));
+thead.append($("<th></th>").html("pm25 Max"));
+thead.append($("<th></th>").html("pm25 Min"));
+let tbody = $("<tbody></tbody>");
+    //iterate through forecast array and append to table body
+    for (let i = 0; i < forecast.length; i++) {
+    let row = $("<tr></tr>");
+  
+    row.append($("<td></td>").html(forecast[i].day));
+    row.append($("<td></td>").html(forecast[i].avg));
+    row.append($("<td></td>").html(forecast[i].max));
+    row.append($("<td></td>").html(forecast[i].min));
+
+    tbody.append(row);
+    }
+        //add tbody to thead
+        table.append(thead).append(tbody);
+        //add table to the DOM
+        $("#more-info").append(table);    
+
+};
 
 //Adds infoWindow in Map
 function postInfo(latitude, longitude, map, contentString) {
@@ -108,12 +117,12 @@ function postInfo(latitude, longitude, map, contentString) {
     });
     infoWindow.setContent(contentString);
     infoWindow.open(map);
-}
+};
 
 //Get Air quality by City
 function getAir(searchTerm){
  
-    fetch(`https://api.waqi.info/feed/${searchTerm}/?token=`)
+    fetch(`https://api.waqi.info/feed/${searchTerm}/?token=` + otherKey)
     .then(response => {
         if (response.ok) {
         $('#js-error-message').empty();
@@ -126,7 +135,7 @@ function getAir(searchTerm){
     .catch(err => {
         $('#js-error-message').text(`Sorry we don't have a reading of that City!`);
     }); 
-}
+};
 let searchTerm
 //listen for search and pass searchTerm into getAir()
 function watchForm() {
@@ -136,6 +145,6 @@ function watchForm() {
         console.log(searchTerm);
         getAir(searchTerm);
 });
-}
+};
 
 $(watchForm);
